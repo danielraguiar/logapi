5fd64dd58e942fe12fc4ad324ec218287f36b51f
Daniel <danielramos.aguiar@gmail.com>
2023-10-22 23:57:08 -0300

    first commit

---
+++ src/main/java/com/danielraguiar/logapi/controller/ClienteController.java
@@ -0,0 +1,8 @@
+package com.danielraguiar.logapi.controller;
+
+import jakarta.persistence.EntityManager;
+
+public class ClienteController {
+
+    private EntityManager entityManager;
+}
---
+++ src/main/java/com/danielraguiar/logapi/domain/model/Cliente.java
@@ -0,0 +1,24 @@
+package com.danielraguiar.logapi.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+}
